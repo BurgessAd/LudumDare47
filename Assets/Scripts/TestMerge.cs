4a1306920a81842281d7a5e781186383deac52ad
CptNick <nicholascthomson@gmail.com>
2020-09-25 20:56:11 +0100

    Update TestMerge.cs

--- Assets/Scripts/TestMerge.cs
+++ Assets/Scripts/TestMerge.cs
@@ -7,7 +7,7 @@ public class TestMerge : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int i = 10;
     }
 
     // Update is called once per frame
