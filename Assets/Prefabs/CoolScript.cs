97ae4c2e5a42d0e2bcff9f17c494b0a4e4d2a354
Grey-Poupon <stephen.northrop@hotmail.com>
2020-10-03 08:05:08 +0100

    Revert "easy merge"
    This reverts commit cd1252a555cf9b33c9430cde52208f6c1f9cf720, reversing
    changes made to d55fc93706539085070bd73e8d9e58873f75d0a8.

--- Assets/Prefabs/CoolScript.cs
+++ Assets/Prefabs/CoolScript.cs
@@ -2,21 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestMerge : MonoBehaviour
+public class CoolScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        int i = 10;
-        int j = 1;
-        int b = 1;
-    }
-
-
-    void GetBlumpkin()
-	{
         
-	}
+    }
 
     // Update is called once per frame
     void Update()
