0a5166487be690b03ef05149b2020a7ac3d100ef
Adam <burgessad1997@hotmail.co.uk>
2020-10-04 16:43:56 +0100

    Pushing Animal Component

--- Assets/Scripts/Gameplay/AnimalComponent.cs
+++ Assets/Scripts/Gameplay/AnimalComponent.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class AnimalComponent : MonoBehaviour
 {
     private AnimalStateHandler ash;
+    [SerializeField]
+    private CowGameManager cowGameManager;
 
     [SerializeField]
     private GameObject animal;
@@ -12,12 +14,13 @@ public class AnimalComponent : MonoBehaviour
     private int speed = 3;
     private int evadeRadius = 10;
     private float evadeSpeedMultiplier = 2;
+    private float jumpHeight = 2;
     [SerializeField]
     private GameObject body;
     float modifier = 1;
     float size = 1;
     Vector3 yeetDir = new Vector3(1, 1, 0);
-
+    
     private Vector2 rotateDirXZ = new Vector2(10f, 10f);
     private float idleTimeout = 5;
     private float idleTimer = 0;
@@ -28,7 +31,7 @@ public class AnimalComponent : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        cowGameManager.cows.Add(gameObject);
         ash = gameObject.AddComponent<AnimalStateHandler>();
         ash.animalComponent = this;
 
@@ -51,7 +54,7 @@ public class AnimalComponent : MonoBehaviour
     void Move()
     {
 
-
+        //Raytrace to keep the cows perp to surface
         RaycastHit hit;
         if (Physics.Raycast(body.transform.position, new Vector3(0, -1, 0), out hit, Mathf.Infinity))
         {
@@ -59,17 +62,19 @@ public class AnimalComponent : MonoBehaviour
         }
 
         float mag = moveDir.magnitude;
-
+		
+        
+        //Keeps cow movement in plane of surface 
         moveDir = Vector3.ProjectOnPlane(moveDir, surfaceNorm);
 
         moveDir = moveDir.normalized * mag * size;
-
+        //Raycast to stop hitting into objects
         if (Physics.Raycast(body.transform.position + moveDir.normalized, moveDir, out hit, 3))
         {
             moveDir *= 0;
             //Debug.DrawRay(body.transform.position, transform.forward*3, Color.yellow);
         }
-
+        //Raycast to stop it jumping of cliffs
         if (Physics.Raycast(body.transform.position + moveDir * 2, new Vector3(0, -1, 0), out hit, 5))
         {
 
@@ -82,7 +87,7 @@ public class AnimalComponent : MonoBehaviour
 
 
 
-
+        //Adds slight movement to body of cow when moving
         if (moveDir != Vector3.zero)
         {
             if (body.transform.rotation.eulerAngles.x >= 92 || body.transform.rotation.eulerAngles.x <= 88)
@@ -100,32 +105,58 @@ public class AnimalComponent : MonoBehaviour
         }
 
         //Debug.DrawRay(body.transform.position, moveDir, Color.black);
-
+        //Rotates body towards direction it will head
         animal.transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(animal.transform.forward, moveDir, Time.deltaTime * modifier, 0));
+        //Sets the velocity 
         animal.GetComponent<Rigidbody>().velocity = new Vector3(moveDir.x * modifier, animal.GetComponent<Rigidbody>().velocity.y, moveDir.z * modifier);
-        
+		if (mag > 0)
+		{
+            if(Random.value>0.95)
+                animal.GetComponent<Rigidbody>().AddForce(Vector3.up, ForceMode.Impulse);
+
+        }
 
     }
 
+    void OnDestroy()
+	{
+        cowGameManager.cows.Remove(gameObject);
+	}
+
+    public void OnLasso()
+    {
+        ash.m_StateMachine.RequestTransition(typeof(AnimalWrangledState));
+    }
+    public void OffLasso()
+	{
+        ash.m_StateMachine.RequestTransition(typeof(AnimalIdleState));
+    }
+
+
+
+    //Called on state change
     public void EnterEvade()
     {
+        //increase speed when evading
         modifier *= evadeSpeedMultiplier;
     }
 
     public void Evading()
     {
+        //Checks to transition to Idle state if further than 1.5 times evade radius
         if ((animal.transform.position - player.transform.position).magnitude > 1.5 * evadeRadius)
         {
             ash.m_StateMachine.RequestTransition(typeof(AnimalIdleState));
             return;
         }
+        //choose direction opposite to player to move away
         moveDir = (animal.transform.position - player.transform.position);
         moveDir = moveDir.normalized;
         moveDir *= speed;
         Move();
     }
 
-
+    //Called when switch to idle state
     public void EnterIdle()
     {
         modifier = 1;
@@ -133,6 +164,7 @@ public class AnimalComponent : MonoBehaviour
 
     public void Idle()
     {
+        //Checks if needs to change state
         if ((animal.transform.position - player.transform.position).magnitude < evadeRadius)
         {
             ash.m_StateMachine.RequestTransition(typeof(AnimalEvadingState));
@@ -140,22 +172,17 @@ public class AnimalComponent : MonoBehaviour
         }
 
         int jump = 0;
+        //checks if it needs to assign new direction or stop moving based on assigned timer
         if (Time.time - idleTimer > idleTimeout)
         {
 
             idleTimeout = 2 + Random.Range(0, 5);
             idleTimer = Time.time;
+            //if wasn't moving, will move
             if (dir == Vector2.zero)
             {
                 dir = Random.insideUnitCircle;
-                if (Random.value > 0.9f)
-                {
-                    jump = 10;
-                }
-                else
-                {
-                    jump = 0;
-                }
+                
             }
             else
             {
@@ -171,8 +198,22 @@ public class AnimalComponent : MonoBehaviour
 
     }
 
-   
+    public void OnWrangled()
+	{
+        gameObject.transform.Find("Leash").gameObject.GetComponent<MeshRenderer>().enabled = true;
+	}
+    public void OffWrangled()
+    {
+        gameObject.transform.Find("Leash").gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
 
+    public void Wrangled()
+	{
+
+	}
+
+   
+    //called when entering Yeeted state
     public void EnterYeet()
 	{
 
